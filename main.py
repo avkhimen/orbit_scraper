13e011d834b6e62c9de120c1e8fc740e1bb20cbf
avkhimen <avkhimen@ualberta.ca>
2020-01-12 16:53:57 -0700

    Added currency argument to determine the currency to scrape to get_input_args()

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ def main():
 	"""Main function to execute all code"""
 
 	# Get input arguments for main.py
-	# Arguments are text notification on/off
-	text_notification_on_or_off = get_input_args().text_notification_on_or_off
+	# Arguments are text notification on/off flag and currency
+	text_notification_on_or_off = get_input_args().text_notification
+	currency = get_input_args().currency
+	
+	# Scraping and database recording will run synchronously
+	# Text message notification will run asynchronously
+
 
 
 
--- support_functions.py
+++ support_functions.py
@@ -1,12 +1,12 @@
 import argparse
 
 def get_input_args():
-    
+    """Returns input arguments for file execution"""
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('text_notification_or_or_off', type = bool, 
-    					default = False, 
-    					help = "Argument to enable text message "\
-    					"notifications. Possible values are 'True' "\
-    					"and 'False'")
+    parser.add_argument('text_notification', 
+                        type = str, default = 'False', 
+                        help = 'Argument to enable text message notifications. Possible values are "True" and "False"')
+    parser.add_argument('currency', 
+                        type = str, default = 'DOGE', 
+                        help = 'Argument for currency which will be scraped')
     return parser.parse_args()
