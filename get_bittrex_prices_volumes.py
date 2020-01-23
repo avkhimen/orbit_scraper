2d21f6a533fd71d4eaebe84a6646c4df560273b5
avkhimen <avkhimen@ualberta.ca>
2020-01-23 10:02:40 -0700

    Refactored get_bittrex[coinsquare]_prices_volumes.py

--- get_bittrex_prices_volumes.py
+++ get_bittrex_prices_volumes.py
@@ -1,44 +1,56 @@
 import requests
-from ast import literal_eval
 from pprint import pprint
 
-buy_or_sell = 'both'
+def main():
+	"""Main program controller"""
+	order_book = get_bittrex_prices_volumes()
+	pprint(order_book)
 
-r = requests.get('https://api.bittrex.com/api/v1.1/public/getorderbook?market=BTC-DOGE&type=' + buy_or_sell)
+def get_bittrex_prices_volumes():
+	"""Returns the dict of latest DOGE price-volume pairs from Bittrex exchange"""
 
-order_book = r.json()['result']
+	r = requests.get('https://api.bittrex.com/api/v1.1/public/getorderbook?market=BTC-DOGE&type=both')
 
-price_volume_dict = {}
+	order_book = r.json()['result']
+
+	price_volume_dict = {}
+
+	#Get ask price and volume
+	i = 1
+
+	while i < 16:
+	    try:
+	        ask_price = order_book['sell'][i]['Rate']
+	        
+	        ask_volume = order_book['sell'][i]['Quantity']
+
+	        price_volume_dict['ask_' + str(i)] = {'volume': ask_volume, 'price': ask_price}
+
+	    except Exception as e:
+	        print(e)
+	        price_volume_dict['ask_' + str(i)] = {'volume': 'scrape_error', 'price': 'scrape_error'}
+	    finally:
+	        i += 1
+
+	#Get bid price and volume
+	i = 1
+
+	while i < 16:
+	    try:
+	        bid_price = order_book['buy'][i]['Rate']
+	        
+	        bid_volume = order_book['buy'][i]['Quantity']
+
+	        price_volume_dict['bid_' + str(i)] = {'volume': bid_volume, 'price': bid_price}
+
+	    except Exception as e:
+	        print(e)
+	        price_volume_dict['bid_' + str(i)] = {'volume': 'scrape_error', 'price': 'scrape_error'}
+	    finally:
+	        i += 1
+
+	return price_volume_dict
 
 
-i = 1
-
-while i < 16:
-    try:
-        ask_price = order_book['sell'][i]['Rate']
-        
-        ask_volume = order_book['sell'][i]['Quantity']
-
-        price_volume_dict['ask_' + str(i)] = {'volume': ask_volume, 'price': ask_price}
-
-    except Exception as e:
-        print(e)
-    finally:
-        i += 1
-
-i = 1
-
-while i < 16:
-    try:
-        bid_price = order_book['buy'][i]['Rate']
-        
-        bid_volume = order_book['buy'][i]['Quantity']
-
-        price_volume_dict['bid_' + str(i)] = {'volume': bid_volume, 'price': bid_price}
-
-    except Exception as e:
-        print(e)
-    finally:
-        i += 1
-
-pprint(price_volume_dict)
+if __name__ == '__main__':
+	main()
--- get_coinsquare_prices_volumes.py
+++ get_coinsquare_prices_volumes.py
@@ -6,58 +6,88 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from pprint import pprint
 
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options)
+def main():
+    """Main program controller"""
+    order_book = get_coinsquare_prices_volumes('https://coinsquare.com/trade?pair=DOGE-BTC')
+    pprint(order_book)
 
-price_volume_dict = {}
+def get_coinsquare_prices_volumes(start_url):
+    """Returns the dict of latest DOGE price-volume pairs from Coinsquare exchange"""
 
-try:
-    driver.get("https://coinsquare.com/trade?pair=DOGE-BTC")
-    time.sleep(10)
-except Exception as e:
-    print("Couldn't load site")
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
 
-i = 1
+    price_volume_dict = {}
 
-while i < 16:
     try:
-        xpath_path = "/html/body/div[1]/div/div[2]/div[3]/div[2]/div"\
-        "[2]/div/div/div[2]/div/div[3]/div[1]/div[" + str(i) + "]/div[1]/div[2]"
-        ask_price = driver.find_element_by_xpath(xpath_path).get_attribute('innerHTML')
-        
-        xpath_path = "/html/body/div[1]/div/div[2]/div[3]/div[2]/div["\
-        "2]/div/div/div[2]/div/div[3]/div[1]/div[" + str(i) + "]/div[1]/div[1]"
-        ask_volume = driver.find_element_by_xpath(xpath_path).get_attribute('innerHTML')
-
-        price_volume_dict['ask_' + str(16-i)] = {'volume': ask_volume, 'price': ask_price}
-
+        driver.get(start_url)
+        time.sleep(10)
     except Exception as e:
-        print(e)
-    finally:
-        i += 1
-        time.sleep(0.0)
+        print('Failed to load site 1st time')
+        try:
+            print('Attemping to load site again')
+            driver.get(start_url)
+            time.sleep(10)
+        except Exception as e:
+            print("Couldn't load site")
+            return False
 
-i = 1
+    #Get ask price and volume
+    i = 1
 
-while i < 16:
-    try:
-        xpath_path = "/html/body/div[1]/div/div[2]/div[3]/div[2]/div[2"\
-        "]/div/div/div[2]/div/div[3]/div[3]/div[" + str(i) + "]/div[1]/div[2]"
-        bid_price = driver.find_element_by_xpath(xpath_path).get_attribute('innerHTML')
-        
-        xpath_path = "/html/body/div[1]/div/div[2]/div[3]/div[2]/div[2]/"\
-        "div/div/div[2]/div/div[3]/div[3]/div[" + str(i) + "]/div[1]/div[1]"
-        bid_volume = driver.find_element_by_xpath(xpath_path).get_attribute('innerHTML')
+    while i < 16:
+        try:
+            xpath_path = "/html/body/div[1]/div/div[2]/div[3]/div[2]/div"\
+            "[2]/div/div/div[2]/div/div[3]/div[1]/div[" + str(i) + "]/div[1]/div[2]"
+            ask_price = driver.find_element_by_xpath(xpath_path).get_attribute('innerHTML')
+            
+            xpath_path = "/html/body/div[1]/div/div[2]/div[3]/div[2]/div["\
+            "2]/div/div/div[2]/div/div[3]/div[1]/div[" + str(i) + "]/div[1]/div[1]"
+            ask_volume = driver.find_element_by_xpath(xpath_path).get_attribute('innerHTML')
 
-        price_volume_dict['bid_' + str(i)] = {'volume': bid_volume, 'price': bid_price}
+            price_volume_dict['ask_' + str(16-i)] = {'volume': ask_volume, 'price': ask_price}
 
-    except Exception as e:
-        print(e)
-    finally:
-        i += 1
-        time.sleep(0.0)
+        except Exception as e:
+            print(e)
+            price_volume_dict['ask_' + str(16-i)] = {'volume': 'scrape_error', 'price': 'scrape_error'}
+        finally:
+            i += 1
+            time.sleep(0.0)
 
-pprint(price_volume_dict)
+    #Get bid price and volume
+    i = 1
 
-driver.close()
+    while i < 16:
+        try:
+            xpath_path = "/html/body/div[1]/div/div[2]/div[3]/div[2]/div[2"\
+            "]/div/div/div[2]/div/div[3]/div[3]/div[" + str(i) + "]/div[1]/div[2]"
+            bid_price = driver.find_element_by_xpath(xpath_path).get_attribute('innerHTML')
+            
+            xpath_path = "/html/body/div[1]/div/div[2]/div[3]/div[2]/div[2]/"\
+            "div/div/div[2]/div/div[3]/div[3]/div[" + str(i) + "]/div[1]/div[1]"
+            bid_volume = driver.find_element_by_xpath(xpath_path).get_attribute('innerHTML')
+
+            price_volume_dict['bid_' + str(i)] = {'volume': bid_volume, 'price': bid_price}
+
+        except Exception as e:
+            print(e)
+            price_volume_dict['bid_' + str(i)] = {'volume': 'scrape_error', 'price': 'scrape_error'}
+        finally:
+            i += 1
+            time.sleep(0.0)
+
+    driver.close()
+
+    return price_volume_dict
+
+class Error(Exception):
+    """Returns basic exception class"""
+    pass
+
+class NoSuchElementException(Error):
+    """Returned when xpath element couldn't be located"""
+    pass
+
+if __name__ == '__main__':
+    main()
